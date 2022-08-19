aae3f87183b902f433af678bd6ae9d361e2e63ff
ssujin-park <pppssujin@gmail.com>
2022-08-20 01:23:15 +0900

    first commit

--- hackathon/accounts/views.py
+++ hackathon/accounts/views.py
@@ -30,7 +30,7 @@ def login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('board')
+            return redirect('home')
         else:
             return render(request, 'login.html', {'error': 'username or password is incorrect.'})
     else:
